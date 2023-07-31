dcb4356fa5cfcd7dfcd874152fa6bf713271c134
Maruf Md Maniruzzaman Abbasi <kuasha@users.noreply.github.com>
2023-07-31 18:50:32 -0400

    Create smartcomputerplayer.py

---
+++ python101/projects/tictactoe/smartcomputerplayer.py
@@ -0,0 +1,48 @@
+class SmartComputerPlayer:
+    def __init__(self, letter):
+        self.letter = letter
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())  # randomly choose one
+        else:
+            # get square based off minimax algorithm
+            square = self.minimax(game, self.letter)['position']
+        return square
+
+    def minimax(self, state, player):
+        max_player = self.letter  # yourself
+        other_player = 'O' if player == 'X' else 'X'
+
+        # first we want to check if the previous move is a winner
+        if state.current_winner == other_player:
+            return {'position': None, 'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (state.num_empty_squares() + 1)}
+        elif not state.empty_squares():  # no empty squares
+            return {'position': None, 'score': 0}
+
+        if player == max_player:
+            best = {'position': None, 'score': -float('inf')}  # each score should maximize
+        else:
+            best = {'position': None, 'score': float('inf')}  # each score should minimize
+        for possible_move in state.available_moves():
+            state.make_move(possible_move, player)
+            sim_score = self.minimax(state, other_player)
+
+            # undo move
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            sim_score['position'] = possible_move  # this represents the move optimal next move
+
+            if player == max_player:  # X is maximizing player
+                if sim_score['score'] > best['score']:
+                    best = sim_score
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+        return best
+
+if __name__ == '__main__':
+    x_player = HumanPlayer('X')
+    o_player = SmartComputerPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game=True)
